16ea88014c6423b704540e326b269f0af581c894
Bobbert <bobbertgaming@outlook.com>
2026-05-20 15:15:08 -0500

    se cambio el perfil

--- app/src/main/java/com/example/unifreelanceapp/Profile.java
+++ app/src/main/java/com/example/unifreelanceapp/Profile.java
@@ -18,7 +18,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 public class Profile extends AppCompatActivity {
 
     Button btnLogout;
-    TextView tvNombre, tvEmail, tvCarrera, tvRol;
+    TextView tvNombre, tvEmail, tvCarrera, tvRol, tvNombreSmall;
     FirebaseAuth mAuth;
     FirebaseFirestore db;
 
@@ -30,6 +30,7 @@ public class Profile extends AppCompatActivity {
 
         btnLogout = findViewById(R.id.btnCerrarSesion);
         tvNombre = findViewById(R.id.tvName);
+        tvNombreSmall = findViewById(R.id.tvNameSmall);
         tvEmail = findViewById(R.id.tvEmail);
         tvCarrera = findViewById(R.id.tvCareer);
         tvRol = findViewById(R.id.tvRole);
@@ -91,6 +92,7 @@ public class Profile extends AppCompatActivity {
                         String role = documentSnapshot.getString("rol");
 
                         tvNombre.setText(name);
+                        tvNombreSmall.setText(name);
                         tvEmail.setText(email);
                         tvRol.setText(role);
 
